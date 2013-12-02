04e39a81a1c4afea8b0b5d2fe6add88e4b9d0952
Juanfra <juanfracastellar@gmail.com>
2013-12-02 10:31:07 +0100

    2/12

--- src/Napalalaki/CardDealer.java
+++ src/Napalalaki/CardDealer.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package Napakalaki;
+package Napalalaki;
 
 import java.util.ArrayList;
 import java.util.Collections;
